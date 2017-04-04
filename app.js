ff4ad94ddb37169f15edf59cd818738f03456c41
Jason <jasonlong98@hotmail.com>
2017-04-04 00:49:46 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const fs = require('fs');
+const yargs = require('yargs');
+const _ = require('lodash');
+
+const notes = require('./notes');
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Yargs: ', argv);
+
+if (command === 'add'){
+    notes.addNote(argv.title, argv.body)
+} else if (command === 'list'){
+    notes.getAll();
+} else if (command === 'read'){
+    notes.getNote(argv.title);
+} else if (command === 'remove'){
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not recognized');
+}
